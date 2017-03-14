262c1f74267d33618c88eb206f7a11b73e82d8e6
Petr Zvoníček <zvonicek@gmail.com>
2017-03-14 11:10:25 +0100

    Add custom bundle identifier specification

--- Sources/Protocols/StoryboardType.swift
+++ Sources/Protocols/StoryboardType.swift
@@ -16,17 +16,18 @@ public protocol StoryboardType {
 public struct StoryboardReference<S: StoryboardType, T> {
 
     public let id: String
+    public let bundle: Bundle?
 
-    public init(id: String) {
+    public init(id: String, bundle: Bundle? = nil) {
+        self.bundle = bundle
         self.id = id
     }
 
     public func instantiate() -> T {
-        if let controller = UIStoryboard(name: S.name, bundle: nil).instantiateViewController(withIdentifier: id) as? T {
+        if let controller = UIStoryboard(name: S.name, bundle: bundle).instantiateViewController(withIdentifier: id) as? T {
             return controller
         } else {
             fatalError("Instantiated controller with \(id) has different type than expected!")
         }
     }
-
 }
