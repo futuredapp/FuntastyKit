923e3dea57fffd2b79bd44bdebd09644856f23ed
Matěj Kašpar Jirásek <matej.jirasek@gmail.com>
2018-02-23 15:26:55 +0100

    Simplify push navigation controller

--- Sources/Architecture/Coordinator.swift
+++ Sources/Architecture/Coordinator.swift
@@ -47,10 +47,8 @@ public enum PresentationStyle {
     case modal
 }
 
-public protocol PushModalCoordinator: DefaultCoordinator {
-    var navigationController: UINavigationController? { get }
-    var presentationStyle: PresentationStyle { get }
-    weak var destinationNavigationController: UINavigationController? { get }
+public protocol PushModalCoordinator: PushCoordinator, ModalCoordinator {
+    var preferredPresentationStyle: PresentationStyle { get }
 }
 
 public extension DefaultCoordinator {
@@ -124,8 +122,8 @@ public extension PushModalCoordinator {
     // By default, to distinguish between modal and push a presence of destinationNavigationController is checked
     // as this is a good heuristics (it's usually not desired to push another navigation controller). This behaviour
     // can be redefined by redeclaring this property on any concrete PushModalCoordinator.
-    var presentationStyle: PresentationStyle {
-        return self.destinationNavigationController != nil ? .modal : .push
+    var preferredPresentationStyle: PresentationStyle {
+        return destinationNavigationController != nil ? .modal : .push
     }
 
     func start() {
@@ -135,26 +133,25 @@ public extension PushModalCoordinator {
 
         configure(viewController: viewController)
 
-        switch presentationStyle {
-        case .modal where destinationNavigationController != nil:
-            navigationController?.present(destinationNavigationController!, animated: animated, completion: nil)
+        switch preferredPresentationStyle {
+        case .modal:
+            let presentedController = destinationNavigationController ?? viewController
+            navigationController.present(presentedController, animated: animated, completion: nil)
         case .push:
-            navigationController?.pushViewController(viewController, animated: animated)
-        default:
-            break
+            navigationController.pushViewController(viewController, animated: animated)
         }
     }
 
     func stop() {
         delegate?.willStop(in: self)
 
-        switch presentationStyle {
+        switch preferredPresentationStyle {
         case .modal:
             viewController?.dismiss(animated: animated) {
                 self.delegate?.didStop(in: self)
             }
         case .push:
-            _ = navigationController?.popViewController(animated: animated)
+            navigationController.popViewController(animated: animated)
             delegate?.didStop(in: self)
         }
     }
