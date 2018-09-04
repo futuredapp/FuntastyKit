c8f55d98915739587abb2cc988a7de35a69eeb13
Matěj Kašpar Jirásek <matej.jirasek@gmail.com>
2018-09-04 10:00:14 +0200

    Add invalid states API adapter tests

--- Tests/FuntastyKitTests/APIAdapterTests.swift
+++ Tests/FuntastyKitTests/APIAdapterTests.swift
@@ -55,7 +55,71 @@ final class APIAdapterTests: XCTestCase {
         wait(for: [expectation], timeout: timeout)
     }
 
-    func testJSONResponse() {
+    func testInvalidDomain() {
+        struct Endpoint: APIEndpoint {
+            let path = "some-failing-path"
+        }
+
+        let delegate = MockupAPIAdapterDelegate()
+        var adapter: APIAdapter = URLSessionAPIAdapter(baseUrl: URL(string: "https://www.tato-stranka-urcite-neexistuje.cz/")!)
+        adapter.delegate = delegate
+        let expectation = self.expectation(description: "Result")
+        adapter.request(data: Endpoint()) { result in
+            expectation.fulfill()
+            if case .value = result {
+                XCTFail("Non-existing domain must fail")
+            }
+        }
+        wait(for: [expectation], timeout: timeout)
+    }
+
+    func testEmptyResult() {
+        struct Endpoint: APIEndpoint {
+            let path = "status/204"
+        }
+
+        let delegate = MockupAPIAdapterDelegate()
+        var adapter = apiAdapter()
+        adapter.delegate = delegate
+        let expectation = self.expectation(description: "Result")
+        adapter.request(data: Endpoint()) { result in
+            expectation.fulfill()
+            if case let .error(error) = result {
+                XCTFail(error.localizedDescription)
+            }
+        }
+        wait(for: [expectation], timeout: timeout)
+    }
+
+    func testCustomError() {
+        struct Endpoint: APIEndpoint {
+            let path = "get"
+        }
+
+        struct CustomError: Error {
+            private init() {}
+
+            init?(data: Data?, response: URLResponse?, error: Error?, decoder: JSONDecoder) {
+                self = CustomError()
+            }
+        }
+
+        let delegate = MockupAPIAdapterDelegate()
+        var adapter: APIAdapter = URLSessionAPIAdapter(baseUrl: URL(string: "http://httpbin.org/")!, customErrorConstructor: CustomError.init)
+        adapter.delegate = delegate
+        let expectation = self.expectation(description: "Result")
+        adapter.request(data: Endpoint()) { result in
+            expectation.fulfill()
+            if case let .error(error) = result {
+                XCTAssertTrue(error is CustomError)
+            } else {
+                XCTFail("Custom error must be returned")
+            }
+        }
+        wait(for: [expectation], timeout: timeout)
+    }
+
+    func testValidJSONResponse() {
         struct TopLevel: Codable {
             let slideshow: Slideshow
         }
@@ -90,7 +154,7 @@ final class APIAdapterTests: XCTestCase {
         wait(for: [expectation], timeout: timeout)
     }
 
-    func testJSONRequestResponse() {
+    func testValidJSONRequestResponse() {
         struct User: Codable, Equatable {
             let uuid: UUID
             let name: String
@@ -123,6 +187,37 @@ final class APIAdapterTests: XCTestCase {
                 XCTAssertEqual(user, response.json)
             case .error(let error):
                 XCTFail(error.localizedDescription)
+            }
+        }
+        wait(for: [expectation], timeout: timeout)
+    }
+
+    func testInvalidJSONRequestResponse() {
+        struct User: Codable, Equatable {
+            let uuid: UUID
+            let name: String
+            let age: UInt
+        }
+
+        struct Endpoint: APIRequestResponseEndpoint {
+            typealias Request = User
+            typealias Response = User
+
+            let data: RequestData
+            let method: HTTPMethod = .post
+            let path = "anything"
+        }
+
+        let user = User(uuid: UUID(), name: "Some Name", age: .random(in: 0...120))
+        let endpoint = Endpoint(request: user)
+        let delegate = MockupAPIAdapterDelegate()
+        var adapter = apiAdapter()
+        adapter.delegate = delegate
+        let expectation = self.expectation(description: "Result")
+        adapter.request(response: endpoint) { result in
+            expectation.fulfill()
+            if case .value = result {
+                XCTFail("Received valid value, decoding must fail")
             }
         }
         wait(for: [expectation], timeout: timeout)
