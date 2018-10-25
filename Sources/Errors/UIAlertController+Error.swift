13b3caa3bc3411d64a658655a8a02158ccbf7753
RomanPodymov <podymfrombryansk@yandex.ru>
2018-10-25 16:45:18 +0200

    replaced alertTitleMessage(error: Error) with 2 methods

--- Sources/Errors/UIAlertController+Error.swift
+++ Sources/Errors/UIAlertController+Error.swift
@@ -10,8 +10,9 @@ import UIKit
 
 public extension UIAlertController {
     convenience init(error: Error, preferredStyle: UIAlertController.Style = .alert) {
-        let (title, message) = UIAlertController.alertTitleMessage(error: error)
-        self.init(title: title, message: message, preferredStyle: preferredStyle)
+        self.init(title: UIAlertController.alertTitle(error: error),
+                  message: UIAlertController.alertMessage(error: error),
+                  preferredStyle: preferredStyle)
         if let error = error as? ResolvableError {
             error.actions.map { $0.alertAction() }.forEach(self.addAction)
             if !error.actions.isEmpty {
@@ -21,17 +22,25 @@ public extension UIAlertController {
         self.addAction(UIAlertAction(title: UIAlertController.okButtonText, style: .default))
     }
 
-    private static func alertTitleMessage(error: Error) -> (String, String) {
+    private static func alertTitle(error: Error) -> String {
         switch error {
         case let error as LocalizedError:
-            return (error.errorDescription ?? defaultErrorText,
-                    error.failureReason ?? error.localizedDescription)
+            return error.errorDescription ?? defaultErrorTitle
         default:
-            return (defaultErrorText, error.localizedDescription)
+            return defaultErrorTitle
         }
     }
 
-    private static var defaultErrorText: String {
+    private static func alertMessage(error: Error) -> String {
+        switch error {
+        case let error as LocalizedError:
+            return error.failureReason ?? error.localizedDescription
+        default:
+            return error.localizedDescription
+        }
+    }
+    
+    private static var defaultErrorTitle: String {
         return NSLocalizedString("Error", comment: "Error")
     }
 
