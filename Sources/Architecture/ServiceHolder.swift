7c6c924d66cc34f0cb85ae283d9bdfbdbee38052
Michal Štembera <stembera.michal@gmail.com>
2019-02-28 15:58:32 +0100

    Enable service to be constructed adhoc within ServiceHolder using scope singleton or transient.

--- Sources/Architecture/ServiceHolder.swift
+++ Sources/Architecture/ServiceHolder.swift
@@ -8,27 +8,69 @@
 
 import Foundation
 
-public protocol Service {}
+public protocol Service: class {}
 public protocol InitializableService: Service {
     init()
 }
 
+public enum ServiceHolderError: Error {
+    /// Service does not implement given protocol
+    case serviceProtocolNotImplemented(protocol: Service.Type, service: Service.Type)
+}
+
+public enum ServiceScope {
+    /// Instance is only created once within ServiceHolder
+    case singleton
+    /// On each get new instance is created using closure provided when added
+    case transient
+}
+
 public class ServiceHolder {
-    private var servicesDictionary: [String: Service] = [:]
+    private enum ServiceRecord {
+        case singleton(instance: Service)
+        case transient(builder: () -> Service)
+    }
+
+    private var servicesDictionary: [String: ServiceRecord] = [:]
 
     public init() {}
 
-    public func add<T>(_ protocolType: T.Type, for concreteType: InitializableService.Type, with name: String? = nil) {
-        self.add(protocolType, for: concreteType.init(), with: name)
+    /// Add IntializableService constructed through empty constructor using the name of the protocol
+    public func add<T: Service, S: InitializableService>(_ protocolType: T.Type,
+                                                         with scope: ServiceScope = .singleton,
+                                                         using serviceType: S.Type) throws {
+        try add(protocolType, with: scope, using: serviceType.init)
     }
 
-    public func add<T>(_ type: T.Type, with name: String? = nil, constructor: () -> Service) {
-        self.add(type, for: constructor(), with: name)
+    /// Add instance as a singleton using the name of the protocol
+    public func add<T: Service, S: Service>(_ protocolType: T.Type,
+                                            using instance: S) throws {
+        try add(protocolType, with: .singleton) {
+            return instance
+        }
     }
 
-    private func add<T>(_ protocolType: T.Type, for instance: Service, with name: String? = nil) {
-        let name = name ?? String(reflecting: protocolType)
-        servicesDictionary[name] = instance
+    /// Add Service constructed through closure provided using the name of the protocol
+    func add<T: Service, S: Service>(_ protocolType: T.Type,
+                                     with scope: ServiceScope = .singleton,
+                                     using builder: @escaping () -> S) throws {
+        guard S.self is T.Type else {
+            throw ServiceHolderError.serviceProtocolNotImplemented(protocol: T.self, service: S.self)
+        }
+        let name = String(reflecting: protocolType)
+        add(name, with: scope, using: builder)
+    }
+
+    /// Add Service constructed through closure provided using the given name
+    func add(_ name: String,
+             with scope: ServiceScope = .singleton,
+             using builder: @escaping () -> Service) {
+        switch scope {
+        case .singleton:
+            servicesDictionary[name] = .singleton(instance: builder())
+        case .transient:
+            servicesDictionary[name] = .transient(builder: builder)
+        }
     }
 
     public func get<T>(by type: T.Type = T.self) -> T {
